9b56556a7f74738c3b96353328142522cfbbc8f4
Anthony Hogg <ahogg@akamai.com>
2017-09-02 16:38:29 +0200

    update elasticsearch dependency (tests and travis)

--- test/check.js
+++ test/check.js
@@ -19,48 +19,61 @@ function getResultsMock() {
 
 var ELASTICSEARCH_INDEX = 'phantomas_test',
 	ELASTICSEARCH_TYPE = 'report_test',
+	ELASTICSEARCH_VERSIONS = [
+		"0.90", "1.7",
+		"2.4",
+		"5.0", "5.1", "5.2", "5.3", "5.4", "5.5", "5.6",
+		"6.0", "6.x",
+		"master"
+	],
 	resultId;
 
-describe('Reporter', () => {
-	it('sends data to elasticsearch', (done) => {
-		var options = {
-			'elasticsearch-index': ELASTICSEARCH_INDEX,
-			'elasticsearch-type': ELASTICSEARCH_TYPE,
-		};
+ELASTICSEARCH_VERSIONS.forEach(esVersion => {
+	describe('Reporter (ES API v' + esVersion + ')', () => {
+		it('sends data to elasticsearch', (done) => {
+			var options = {
+				'elasticsearch-index': ELASTICSEARCH_INDEX,
+				'elasticsearch-type': ELASTICSEARCH_TYPE,
+				'elasticsearch-api': esVersion
+			};
 
-		reporter(getResultsMock(), [], options).render((err, data) => {
-			assert.ok(err === undefined);
+			reporter(getResultsMock(), [], options).render((err, data) => {
+				console.log(err);
+				assert.ok(err === undefined);
 
-			console.log('Result stored under ID: ' + data.id);
-			resultId = data.id;
+				console.log('Result stored under ID: ' + data.id);
+				resultId = data.id;
 
-			done();
+				done();
+			});
 		});
 	});
-});
 
-// http://127.0.0.1:9200/_cat/indices?v
-// http://127.0.0.1:9200/phantomas_test/_search?q=foo:bar&sort=reportDate:desc&size=1
-// http://127.0.0.1:9200/phantomas_test/report_test/AVdxouBSKV-iX4VpGWnz
-describe('elasticsearch', () => {
-	it('returns stored results', (done) => {
-		var elasticsearch = require('elasticsearch'),
-			client = new elasticsearch.Client(),
-			results = getResultsMock();
+	// http://127.0.0.1:9200/_cat/indices?v
+	// http://127.0.0.1:9200/phantomas_test/_search?q=foo:bar&sort=reportDate:desc&size=1
+	// http://127.0.0.1:9200/phantomas_test/report_test/AVdxouBSKV-iX4VpGWnz
+	describe('elasticsearch (ES API v' + esVersion + ')', () => {
+		it('returns stored results', (done) => {
+			var elasticsearch = require('elasticsearch'),
+				client = new elasticsearch.Client({
+					apiVersion: esVersion
+				}),
+				results = getResultsMock();
 
-		// https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference-1-4.html#api-get-1-4
-		client.get({'index': ELASTICSEARCH_INDEX, 'type': ELASTICSEARCH_TYPE, 'id': resultId}, (err, resp) => {
-			assert.ok(err === undefined);
-			console.log(resp);
+			// https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference-1-4.html#api-get-1-4
+			client.get({'index': ELASTICSEARCH_INDEX, 'type': ELASTICSEARCH_TYPE, 'id': resultId}, (err, resp) => {
+				assert.ok(err === undefined);
+				console.log(resp);
 
-			var res = resp._source;
+				var res = resp._source;
 
-			assert.equal(res.url, results.getUrl());
-			assert.equal(res.requests, results.getMetric('requests'));
-			assert.equal(res.loadTime, results.getMetric('loadTime'));
-			assert.equal(res.foo, results.getMetric('foo'));
+				assert.equal(res.url, results.getUrl());
+				assert.equal(res.requests, results.getMetric('requests'));
+				assert.equal(res.loadTime, results.getMetric('loadTime'));
+				assert.equal(res.foo, results.getMetric('foo'));
 
-			done();
+				done();
+			});
 		});
 	});
 });
